fcf5ef7acf8080a83413687914501ce2164906d1
Arshad <arshad.syed.anwar@gmail.com>
2022-01-03 03:45:33 +0000

    second commit

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ $the_username = $POST["username"];
 if($the_username!=""){
   print "$the_username";
 }else{
-  print "<form method='post' action='/test/index.php'>";
+  print "<form method='post' action='/'>";
   print "Username<br>";
   print "<input type='text' name='username' size='25'><br>";
   print "<input type='submit' value='Go'>";
